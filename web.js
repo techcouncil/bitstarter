e3ed2a6c630a81074bcf8dcee352f29261ba0054
techcouncil <techcouncil@appcoach.com>
2013-07-10 11:06:47 +0200

    Reading response from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexBuffer = fs.readFileSync('index.html');
+    response.send(indexBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
